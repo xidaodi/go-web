8fe1c25779b629b79ac0b96d5e6f029f2a3b2543
Summer Xia <35069982+xidaodi@users.noreply.github.com>
2021-11-30 12:16:15 +0800

    Update hello.go

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ func main() {
  r := gin.Default()
  r.GET("/ping", func(c *gin.Context) {
      c.JSON(200, gin.H{
-         "message": "hello CP---second trigger",
+         "message": "hello CP---third trigger",
      })
  })
  r.Static("/index","/root/")
